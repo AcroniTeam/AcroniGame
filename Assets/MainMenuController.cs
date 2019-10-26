c7719b00579a25cf7d03b5313fc43ea36c551ac1
FOOTI <37518682+Foot-G@users.noreply.github.com>
2019-10-26 10:16:59 -0300

    Correção de bugs no protótipo do menu principal
    Commit anterior:
    Mota - Primeiro Protótipo do Menu Principal

---
+++ Assets/MainMenuController.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MainMenuController : MonoBehaviour
+{
+    public Animator animator;
+
+    bool isOpen = false;
+    public void OnClick()
+    {
+        animator.SetTrigger("CanAnimate");
+        isOpen = (isOpen) ? false : true;
+        animator.SetBool("isOpen", isOpen);
+    }
+
+}
